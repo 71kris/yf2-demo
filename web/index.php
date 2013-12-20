4b54b914956637e17fb3f52ae85dcb0e831f5b14
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-12-20 15:30:30 -0600

    Updating for NewRelic tracking

--- web/index.php
+++ web/index.php
@@ -1,7 +1,10 @@
 <?php
 
+if (extension_loaded('newrelic'))
+  newrelic_set_appname('YF2 Demo');
+
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
